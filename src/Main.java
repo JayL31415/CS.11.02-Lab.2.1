48f2ad2c4722cf4ba8af44d5ab72dad1f95b0776
J <jaylauhc@gmail.com>
2023-09-07 22:09:39 +0800

    complete

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,89 @@
 public class Main {
 
+
+//------testing
     public static void main(String[] args) {
+        int result = add(1,2);
+        System.out.println(result);
+
+        int secondresult = add(2, 3, 4, 5);
+        System.out.println(secondresult);
+
+        String thirdresult = morningGreeting("Toby Fox");
+        System.out.println(thirdresult);
+
+        String fourthresult = afternoonGreeting("Mac Miller");
+        System.out.println(fourthresult);
+
+        String fithresult = triple("oohbaby");
+        System.out.println(fithresult);
+
+        double sixthresult = half(53);
+        System.out.println(sixthresult);
+
+        double seventhresult = roundPositiveValueToNearestInteger(8.49);
+        System.out.println(seventhresult);
+
+        double eighthresult = roundNegativeValueToNearestInteger(-9.49);
+        System.out.println(eighthresult);
 
     }
+//1. adding 2 things together)
+    public static int add(int a, int b) {
+        return a+b;
+
+    }
+//2.  adding 4 things together
+    public static int add(int c, int d, int e, int f) {
+        int cd = add(c,d);
+        int cde = add(cd,e);
+        int cdef = add(cde,f);
+        return cdef;
+    }
+
+//3. morningGreeting
+
+    public static String morningGreeting(String name) {
+        String greeting = "早上好, " + name + "!";
+        return greeting;
+    }
+
+//4. afternoonGreeting
+
+    public static String afternoonGreeting(String secondname) {
+        String secondgreeting = "下午好, "+ secondname + "!";
+        return secondgreeting;
+        }
+//5. triple
+    public static String triple(String word){
+        String tripleword = word + word + word;
+        return tripleword;
+    }
+//6. half
+    public static double half(double givenNumber){
+        double givenAnswer = givenNumber/2;
+        return givenAnswer;
+    }
+
+//7. roundPositiveValueToNearestInteger
+    public static double roundPositiveValueToNearestInteger(double givenNumberTwo){
+        double givenHalfAnswerTwo = givenNumberTwo + 0.5;
+        int givenAnswerTwo = (int) givenHalfAnswerTwo;
+        return givenAnswerTwo;
+    }
+
+    public static double roundNegativeValueToNearestInteger(double givenNumberThree){
+        double givenHalfAnswerThree = (givenNumberThree - 0.5);
+        int givenAnswerThree = (int) givenHalfAnswerThree;
+        return givenAnswerThree;
+    }
+
+
+
+
+
+
+
 
     // 1. add
     
@@ -20,4 +101,4 @@ public class Main {
 
     // 8. roundNegativeValueToNearestInteger
 
-}
+ }
